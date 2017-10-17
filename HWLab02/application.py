19aa13ce2d59acdf8c03170f6526c7beb61acaec
Matko Gabi <ytgabi98@gmail.com>
2017-10-17 13:16:44 +0300

    fixed 1 feature and added one more to HWLab02

--- HWLab02/application.py
+++ HWLab02/application.py
@@ -23,12 +23,17 @@ def check010Range(numbers):
             max = length
             start = len(numbers) - max
 
-    print(numbers[start: start + max])
+    printList(numbers[start: start + max])
 
     if length > 0:
         return True
     return False
 
+def calculateSum(list):
+    s = 0
+    for i in range(0, len(list)):
+        s += list[i]
+    return s
 
 def checkElementsSum(numbers):
     """
@@ -36,31 +41,20 @@ def checkElementsSum(numbers):
     :param numbers: the list that needs to be verified
     :return: true if the condition from above is aquired, false if not
     """
-    start = 0
-    length = 0
-    max = 0
-    s = 0
+    maxLen = 0
+    list = []
     for i in range(0, len(numbers)):
-        if s < 10 and numbers[i] <= 10:
-            length += 1
-        else:
-            if s == 10:
-                if length > max:
-                    max = length
-                    start = i - max
-        length = 0
+        for j in range(i+1, len(numbers)+1):
+            if calculateSum(numbers[i:j]) == 10:
+                if len(numbers[i:j]) > maxLen:
+                    maxLen = len(numbers[i:j])
+                    list = numbers[i:j]
 
-    if length > 0:
-        if length > max:
-            max = length
-            start = len(numbers) - max
-
-    print(numbers[start: start + max])
-
-    if length > 0:
+    if maxLen > 0:
+        printList(list)
         return True
-    return False
 
+    return False
 
 def checkDigits(nr1, nr2):
     """
@@ -108,55 +102,96 @@ def checkConsecutiveCommonDigits(numbers):
                 start = i - max
             length = 0
 
+    if start != start + max:
+        printList(numbers[start: start + max + 1])
+        return True
+    return False
+
+def getSign(nr1, nr2):
+    if nr1 - nr2 < 0:
+        return 0
+    return 1
+
+def printList(list):
+    print(list)
+
+def checkSignDifference(numbers):
     '''
+    Checks if in numbers list exists a sub-sequence where
+    difference between consecutive number pairs have different
+    signs
+    :param numbers: the list of numbers
+    :return: true if there exists such a sub-sequence, false if not
+    '''
+    start = 0
+    length = 0
+    max = 0
+
+    lastSign = getSign(numbers[0], numbers[1])
+    #print(numbers)
+    #0 - last sign was -, 1 - last sign was +
+    for i in range(1, len(numbers) - 1):
+        currSign = getSign(numbers[i], numbers[i + 1])
+        #print("last sign = ", lastSign, ", currSign = ", currSign)
+        #print(numbers[i], " ", numbers[i + 1])
+        if currSign != lastSign:
+            length += 1
+        else:
+            if length > max:
+                max = length
+                start = i - max
+            length = 0
+        lastSign = currSign
+
+
     if length > 0:
         if length > max:
             max = length
             start = len(numbers) - max - 1
-    '''
+
     if start != start + max:
-        print(numbers[start: start + max + 1])
+        printList(numbers[start - 1: start + max + 1])
         return True
     return False
-    '''
-    if length > 0:
-        print(numbers[start: start + max + 1])
-        return True
-    return False
-    '''
 
 def main():
     numbers = []
     word = "/0"
     print("Enter an array of numbers.\nPress enter one more time to stop reading.\n")
-
+    '''
     while word != "":
         word = input()
         if(word.isnumeric()):
             numbers.append(int(word))
 
     print("The number list is: ", numbers)
-
-    #numbers = [10, 1, 5, 2, 8, 12, 5, 3, 2, 19, 1, 2, 3, 5, 2, 4]
+    '''
+    numbers = [11, 2, 8, 0, 1, 1, 32, 9, 3]
+    #output: 1, 3, 2, 10, 5
     printMenu()
     option = int(readOption())
     result = True
+
     if option == 1:
         result = check010Range(numbers)
     elif option == 2:
         #not working properly
-        return
+        #return
         result = checkElementsSum(numbers)
     elif option == 3:
         result = checkConsecutiveCommonDigits(numbers)
     elif option == 4:
+        result = checkSignDifference(numbers)
+    elif option == 5:
+        print(numbers)
+    elif option == 6:
         return
 
-    if result:
-        print("The array you typed have the ", option, " property.")
-    else:
-        print("The array you typed doesn't have the ", option, " property.")
-
+    if option <= 4:
+        if result:
+            print("The array you typed have the ", option, " property.")
+        else:
+            print("The array you typed doesn't have the ", option, " property.")
 
 
 def clearConsole():
@@ -167,14 +202,13 @@ def clearConsole():
     clear = lambda: os.system('cls')
     clear()
 
-
 def readOption():
     """
     Reads the desired option from a listed menu
     :return: selected option ( from 1 to 4 )
     """
     option = input()
-    while not option.isnumeric() or int(option) < 1 or int(option) > 4:
+    while not option.isnumeric() or int(option) < 1 or int(option) > 6:
         #clearConsole()
         print("Invalid option. Try again.\n")
         printMenu()
@@ -183,16 +217,18 @@ def readOption():
     #clearConsole()
     return option
 
-
 def printMenu():
     """
     Prints a menu
     :return: nothing
     """
-    print("Which properties do you want to check on this list?")
+    print("Which property do you want to check on this list?")
     print("1. All elements are in the [0, 10] range")
     print("2. The sum of all elements is 10 - not working right now")
     print("3. All consecutive number pairs have at least 2 common digits")
-    print("4. Close the app")
+    print("4. Differences between consecutive number pairs have different signs")
+    print("")
+    print("5. Just print the list")
+    print("6. Close the app")
 
 main()
