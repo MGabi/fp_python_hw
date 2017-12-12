4e55b49b2c01875928d3f2fbf8d3c972e47efe55
Matko Gabi <ytgabi98@gmail.com>
2017-12-12 12:25:26 +0200

    added bogo sort

--- HWLab0507/domain/entities/my_dict.py
+++ HWLab0507/domain/entities/my_dict.py
@@ -3,30 +3,61 @@
     @email:  ytgabi98@gmail.com
     @date:   12/11/2017 18:51
 """
+import random
 from collections import OrderedDict
 
 from copy import deepcopy
 
 
 class MyDict(dict):
+
     def __init__(self):
         super(MyDict, self).__init__()
 
-    # GNOME SORT
-    def sort(self, func, lst):
-        poz = 0
-        listLen = len(lst)
-        while poz < listLen:
-            # if i and lst[i] < lst[i - 1]:
-            if poz > 0 and func(lst[poz], lst[poz-1]):
-                lst[poz], lst[poz - 1] = lst[poz - 1], lst[poz]
-                poz -= 1
-            else:
-                poz += 1
-        return lst
+    def __setitem__(self, key, value):
+        super(MyDict, self).__setitem__(key, value)
 
-    def bongoSort(self, fun, lst):
-        pass
+    def __delitem__(self, key):
+        super(MyDict, self).__delitem__(key)
+
+    def __next__(self):
+        super(MyDict, self).__next__()
+
+    def __iter__(self):
+        super(MyDict, self).__iter__()
+
+    # GNOME SORT
+    # def sort(self, func, lst):
+    #     poz = 0
+    #     listLen = len(lst)
+    #     while poz < listLen:
+    #         # if i and lst[i] < lst[i - 1]:
+    #         if poz > 0 and func(lst[poz], lst[poz-1]):
+    #             lst[poz], lst[poz - 1] = lst[poz - 1], lst[poz]
+    #             poz -= 1
+    #         else:
+    #             poz += 1
+    #     return lst
+
+    @staticmethod
+    def sort(func, collection):
+        """Pure implementation of the bogosort algorithm in Python
+        :param collection: some mutable ordered collection with heterogeneous
+        comparable items inside
+        :return: the same collection ordered by ascending
+        """
+        def isSorted(collection):
+            if len(collection) < 2:
+                return True
+            for i in range(len(collection) - 1):
+                if func(collection[i+1], collection[i]):
+                #if collection[i] > collection[i + 1]:
+                    return False
+            return True
+
+        while not isSorted(collection):
+            random.shuffle(collection)
+        return collection
 
     def filter(self, func):
         finalList = []
